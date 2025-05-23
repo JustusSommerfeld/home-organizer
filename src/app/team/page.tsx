7d0997a21c1f7a05b04789588d40e716274df17a
JustusSommerfeld <justus.sommerfeld24@gmail.com>
2025-05-23 23:13:39 +0200

    Added Team-Page

---
+++ src/app/team/page.tsx
@@ -0,0 +1,56 @@
+const people = [
+  {
+    name: "Justus Sommerfeld",
+    role: "Co-Founder / CEO",
+    imageUrl:
+      "/profile.jpg",
+  },
+    {
+    name: "Lukas Sommerfeld",
+    role: "Co-Founder / CTO",
+    imageUrl:
+        "/profile.jpg",
+    }
+  // More people...
+];
+
+export default function Team() {
+  return (
+    <div className="bg-white min-h-screen py-24 sm:py-32">
+      <div className="mx-auto grid max-w-7xl gap-20 px-6 lg:px-8 xl:grid-cols-3">
+        <div className="max-w-xl">
+          <h2 className="text-3xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-4xl">
+            Meet our leadership
+          </h2>
+          <p className="mt-6 text-lg/8 text-gray-600">
+            We’re a dedicated team committed to helping you organize your home and simplify your daily life. Our passion is creating solutions that make home management easier and more enjoyable for everyone.
+          </p>
+        </div>
+        <ul
+          role="list"
+          className="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2"
+        >
+          {people.map((person) => (
+            <li key={person.name}>
+              <div className="flex items-center gap-x-6">
+                <img
+                  alt=""
+                  src={person.imageUrl}
+                  className="size-16 rounded-full"
+                />
+                <div>
+                  <h3 className="text-base/7 font-semibold tracking-tight text-gray-900">
+                    {person.name}
+                  </h3>
+                  <p className="text-sm/6 font-semibold text-indigo-600">
+                    {person.role}
+                  </p>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
