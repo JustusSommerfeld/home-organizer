b913cb4c8f18aa2151d5b7dda08ab08da4a94130
JustusSommerfeld <justus.sommerfeld24@gmail.com>
2025-05-23 20:23:23 +0200

    Changed header to logo

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -9,6 +9,7 @@ import {
   faUser,
 } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
+import Image from "next/image";
 
 
 export default function Dashboard() {
@@ -16,7 +17,14 @@ export default function Dashboard() {
     <div className="flex min-h-screen bg-gray-50">
       {/* Sidebar */}
       <nav className="w-64 bg-white shadow-md flex flex-col py-8 px-4">
-      <h1 className="text-3xl font-bold mb-10 text-center">Dashboard</h1>
+      {/*<h1 className="text-3xl font-bold mb-10 text-center">Dashboard</h1>*/}
+      <Image
+        src="/logo.svg"
+        alt="Logo"
+        width={110}
+        height={100}
+        className="mx-auto mb-10"
+      />
       <ul className="space-y-4">
         <li>
         <div className="flex items-center space-x-4 p-3 rounded-lg hover:bg-gray-100 cursor-pointer transition text-gray-300">
